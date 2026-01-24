9fe7126fd3d300b4a918dbd5427b1f0d13241531
Muniraja Mada <munirajamada@protonmail.com>
2026-01-24 19:09:01 +0530

    chore: bump version to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.HyntixHQ"
             artifactId = "KotlinPdfium"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
